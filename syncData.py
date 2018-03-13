9d25f0746875a2f884caac47c61caaeec688442e
Joffrey Gonin <Joffrey.Gonin@gmail.com>
2018-03-13 12:42:44 +0100

    Just few changes.. not rely important

--- syncData.py
+++ syncData.py
@@ -3,16 +3,31 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from hbp_validation_framework import ModelCatalog
 
-hbp_username = "shailesh"
+# hbp_username = "shailesh"
+hbp_username = "jgonin"
 
 scope = ['https://spreadsheets.google.com/feeds']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('VF-DataSync-8e2e5296f1c5.json', scope)
-
+if hbp_username == "shailesh":
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('VF-DataSync-8e2e5296f1c5.json', scope)
+elif hbp_username == "jgonin":
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/joffrey/Private/UNIC-VFSyncData-f5472b66de6f.json', scope)
+else :
+    pass
+    ###TODO raise error
 gc = gspread.authorize(credentials)
 
 # sc = gc.open("testing_python")
-sc = gc.open_by_url("https://docs.google.com/spreadsheets/d/1gavQH_2JqujQ-7HPhxcFRD_x0BHC9zvCKDWqU1rK-s4/edit#gid=0")
+sc = gc.open("Copiedetesting_python")
+
+# sc = gc.open_by_key('1gavQH_2JqujQ-7HPhxcFRD_x0BHC9zvCKDWqU1rK-s4')
+
+if hbp_username == "shailesh":
+    sc = gc.open_by_url("https://docs.google.com/spreadsheets/d/1gavQH_2JqujQ-7HPhxcFRD_x0BHC9zvCKDWqU1rK-s4/edit#gid=0")
+elif hbp_username == "jgonin":
+    pass
+    # sc = gc.open_by_url("https://docs.google.com/spreadsheets/d/1gavQH_2JqujQ-7HPhxcFRD_x0BHC9zvCKDWqU1rK-s4/edit#gid=2033167530")
+    sc = gc.open_by_url("https://docs.google.com/spreadsheets/d/1pdRXthZtiDmeY4ndraFdPtUnb9BLCToKsYfeczkAoQg/edit#gid=2033167530")
 
 # Selecting a worksheet by title
 worksheet = sc.worksheet("Basal ganglia")
@@ -27,17 +42,21 @@ headers_list = worksheet_data[0]
 
 model_library = ModelCatalog(hbp_username, environment="dev")
 
-type_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "artefact type"][0]
+artefact_type_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "artefact type"][0]
+author_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "author"][0]
+cell_type_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "cell type"][0]
+region_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "brain region"][0]
+species_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "species"][0]
+desc_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "description"][0]
+name_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "name"][0]
+
 for row_entry in worksheet_data[1:]:
-    if row_entry[type_col_ind].lower() in ["model", "model collection"]:
+    
+    if row_entry[artefact_type_col_ind].lower() in ["model", "model collection"]:
         # set model host app ID
         model_app_id = "39968"
 
-        # set model name
-        model_name = # TODO
-
         # set author
-        author_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "author"][0]
         model_author = row_entry[author_col_ind]
         if model_author == "":
             model_author = "Unknown"
@@ -46,26 +65,28 @@ for row_entry in worksheet_data[1:]:
         model_org = ""
 
         # set cell type
-        type_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "cell type"][0]
-        model_cell_type = row_entry[type_col_ind]
+        model_cell_type = row_entry[cell_type_col_ind]
 
         # set model type
-        if row_entry[type_col_ind].lower() == "model":
+        if row_entry[artefact_type_col_ind].lower() == "model":
             model_type = "Single Cell"
         else:
             model_type = "Network"
 
         # set model brain region
-        region_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "brain region"][0]
         model_brain_region = row_entry[region_col_ind]
 
         # set model species
-        species_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "species"][0]
         model_species = row_entry[species_col_ind]
 
         # set model description
-        desc_col_ind = [i for i,head in enumerate(headers_list) if head.lower() == "description"][0]
         model_desc = row_entry[desc_col_ind]
+
+        # set model name
+        # row_entry[name_col_ind]
+        model_name = model_cell_type+model_species+model_brain_region
+
+        print model_name
 
         """
         # Register model without instance
